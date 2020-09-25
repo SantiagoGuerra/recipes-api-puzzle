a2c3a1cad490952b51c408ffb9afc8e6326ad474
Santiago Guerra <guerrasantiago19@gmail.com>
2020-09-25 03:12:18 -0500

    Create Token Type

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,7 +1,18 @@
-import { Query, Resolver } from 'type-graphql'
+import { Query, Resolver, Mutation, ObjectType, Field } from 'type-graphql'
+
+
+@ObjectType()
+class TokenType {
+  @Field(() => String)
+  token!: string
+}
+
 
 @Resolver()
 export class UserResolver {
+
+
+  @Mutation(() => TokenType)
 
   @Query(() => String)
 	hi():string {
