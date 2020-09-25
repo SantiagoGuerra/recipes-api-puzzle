c6166add8dc6601da9bb18d8ac95bb5310a910d1
Santiago Guerra <guerrasantiago19@gmail.com>
2020-09-25 02:48:36 -0500

    Set up linter

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,18 +1,19 @@
 import {createConnection} from 'typeorm'
 import path from 'path'
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function connect() {
-  await createConnection({
-    type:"postgres",
-    host: process.env.HOST || "localhost",
-    port: 5432,
-    username: process.env.DB_USERNAME || "puzzle",
-    password: process.env.DB_PASSWORD || "1234567",
-    database: process.env.DB_NAME || "puzzle_challenge",
-    entities: [
-      path.join(__dirname, '../entity/**/**.ts')
-    ],
-    synchronize: true
-  });
-  console.log('Database is Connected')
+	await createConnection({
+		type:'postgres',
+		host: process.env.HOST || 'localhost',
+		port: 5432,
+		username: process.env.DB_USERNAME || 'puzzle',
+		password: process.env.DB_PASSWORD || '1234567',
+		database: process.env.DB_NAME || 'puzzle_challenge',
+		entities: [
+			path.join(__dirname, '../entity/**/**.ts')
+		],
+		synchronize: true
+	})
+	console.log('Database is Connected')
 }
